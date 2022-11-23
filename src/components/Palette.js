eb6a5582f4afa1b5f25477b22713e883e7c8b79d
Jean Fraga <jeandevjs@gmail.com>
2022-11-23 15:49:39 -0300

    feat: creating a Palette Component

---
+++ src/components/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  
+  render () {
+    return (
+      <div className='Palette'>
+        {/*NavBar*/}
+        <div className='Palette-colors'>{/*bunch of color boxes*/}</div>
+        {/*footer eventually*/}
+      </div>
+    )
+  }
+}; 
