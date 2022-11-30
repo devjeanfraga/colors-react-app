6f7d3c71164c1528e850bf6e620bb122cd07a1ba
Jean Fraga <jeandevjs@gmail.com>
2022-11-30 10:02:21 -0300

    feat: add CopyClipboard component

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './ColorBox.css';
 
 export default class ColorBox  extends Component {
@@ -9,7 +10,19 @@ export default class ColorBox  extends Component {
         ColorBox  contains an unique color, 
         in addtion, "more"  wrapped inside span
         has all variable about that color
+
+        ----CLICKBOARD FUNCTION---- 
+        //get the text field
+        var copyText =  document.getElementById("any-id")
+        
+        //select the text field
+        copyText.select();
+        copyText.setSelectionRange(0,99999); // For mobile devices
+
+        //copy the text inse the text field
+        navigator.clipboard.writeText(copyText.value); 
       */ 
+     <CopyToClipboard text={background}>
       <div className="ColorBox" style={{ background: background }}>
         <div className="copy-container">
           <div className="box-content">
@@ -19,6 +32,7 @@ export default class ColorBox  extends Component {
         </div>
         <span className="see-more">MORE</span>
       </div>
+     </CopyToClipboard>
     );
   };
 };
