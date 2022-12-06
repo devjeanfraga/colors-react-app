4855226e28052cf324e4000325f009d431908616
Jean Fraga <jeandevjs@gmail.com>
2022-12-06 11:18:56 -0300

    add palette fotter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import seedColors from "./seed-colors";
+import Palette from "./components/Palette";
+import {generatePalette} from "./colorHelpers";  
+
 
 function App() {
+  //console.log(generatePalette(seedColors[6]))
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Palette palette={generatePalette(seedColors[6])}/>
     </div>
   );
 }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -20,9 +20,9 @@ export default class Palette extends Component {
   }
 
   render () {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
-    const colorBoxes = colors[level].map(color => ( <ColorBox background={color[format]} name={color.name}/> ));
+    const colorBoxes = colors[level].map(color => ( <ColorBox background={color[format]} name={color.name}  key={color.id} /> ));
 
     return (
       <div className='Palette'>
@@ -31,9 +31,12 @@ export default class Palette extends Component {
           changeLevel={this.changeLevel}
           handleChange ={this.changeFormat}
         />
-        {/*NavBar*/}
+      
         <div className='Palette-colors'>{colorBoxes}</div>
-        {/*footer eventually*/}
+        <footer className="Pallet-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     )
   }
