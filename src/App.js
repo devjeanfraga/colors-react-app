c1ad9ffdf8cda92a6513cd4883c6234a2e153769
Jean Fraga <jeandevjs@gmail.com>
2022-12-06 11:48:05 -0300

    chore: add react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Route, Routes} from "react-router-dom"
 import seedColors from "./seed-colors";
 import Palette from "./components/Palette";
 import {generatePalette} from "./colorHelpers";  
@@ -6,10 +7,14 @@ import {generatePalette} from "./colorHelpers";
 function App() {
   //console.log(generatePalette(seedColors[6]))
   return (
+    <Routes>
+      <Route exact path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
+      <Route exact path="/palette/:id" element={<h1>PALETTE ID GOES HERE</h1>} />
+    </Routes>
     
-    <div className="App">
-        <Palette palette={generatePalette(seedColors[6])}/>
-    </div>
+    // <div className="App">
+    //     <Palette palette={generatePalette(seedColors[6])}/>
+    // </div>
   );
 }
 
