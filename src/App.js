500f27773023596a5c807334c9dd36ddb10bfc88
Jean Fraga <jeandevjs@gmail.com>
2022-12-09 23:33:18 -0300

    buildind singleColorPalette

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App() {
       <Route 
         exact 
         path="/palette/:paletteId/:colorId" 
-        render={() => <SingleColorPalette/>} 
+        render={(routeProps) => <SingleColorPalette 
+          colorId={routeProps.match.params.colorId}
+          palette={generatePalette(findPalette(routeProps.match.params.paletteId))}
+        />} 
       />
     </Switch>
     
--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -1,7 +1,25 @@
 import React, {Component} from "react"
 
 class SingleColorPalette extends Component {
+  constructor (props) {
+    super(props);
+    this.shades = this.getColorShades(this.props.palette, this.props.colorId); 
+  }
+
+  getColorShades (palette, colorToFilterBy) {
+    let shades = [];
+    let allColors = palette.colors;
+
+    for ( let key in allColors ) {
+      shades = shades.concat(
+        allColors[key].filter( item => item.id === colorToFilterBy)
+      );
+    };
+    return shades;
+  }; 
+
   render () {
+    //console.log(this.props.palette, this.props.colorId); 
     return (
       <div>
         <h1>Single Color Palette</h1>
