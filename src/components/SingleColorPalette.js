701eecf8cc4cb9711305bcd08398148831a19dad
Jean Fraga <jeandevjs@gmail.com>
2022-12-12 15:07:37 -0300

    feat|style: add colorBox and refactor

--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react"
+import { Link } from "react-router-dom";
+import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 
 class SingleColorPalette extends Component {
   constructor (props) {
     super(props);
-    this.shades = this.getColorShades(this.props.palette, this.props.colorId); 
+    this.shades = this.getColorShades(this.props.palette, this.props.colorId);
+    this.changeFormat = this.changeFormat.bind(this); 
+    this.state = {format: "hex"};  
   }
 
   getColorShades (palette, colorToFilterBy) {
@@ -15,14 +21,29 @@ class SingleColorPalette extends Component {
         allColors[key].filter( item => item.id === colorToFilterBy)
       );
     };
-    return shades;
+    
+    return shades.slice(1);
   }; 
 
+  changeFormat (val) {
+    this.setState({format: val}); 
+  };
+
   render () {
-    //console.log(this.props.palette, this.props.colorId); 
+    const {format} = this.state;
+    const {paletteName, emoji, id} = this.props.palette;
+    const colorBoxes = this.shades.map(color => ( <ColorBox key={color.name} name={color.name} background={color[format]} showLink={false}/>)); 
+    
     return (
-      <div>
-        <h1>Single Color Palette</h1>
+      <div className='SingleColorPalette Palette'>
+        <Navbar handleChange={this.changeFormat} isSingleColor={false}/>
+        <div className='Palette-colors'>
+          {colorBoxes}
+          <div className='go-back ColorBox'>
+            <Link to={`/palette/${id}`}className="back-button">Go Back</Link>
+          </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji}/>
       </div>
     );
   };
